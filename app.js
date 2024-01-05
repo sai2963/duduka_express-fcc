ed4c048ae40dbad5edfd7997cda03f030091ea49
sai2963 <venkatsai0629@gmail.com>
2024-01-05 20:35:13 +0530

    express basics done in app.js

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-function alpha(){
-    console.log('Express Tutorial')
-}
-alpha();
+const express=require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+res.send('hello World')
+})
+app.get('/about',(req,res)=>{
+    res.send('Home Page')
+})
+app.all('*',(req,res)=>{
+    res.status(404).send("<h1>Resource not found</h1>")
+})
+
+app.listen(5000,()=>{
+    console.log('server is runnimg at port 5000')
+}) 
