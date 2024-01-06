58db93d3b433ec00b52108aed402a7e54760b061
sai2963 <venkatsai0629@gmail.com>
2024-01-06 22:32:34 +0530

    files added to the server

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express=require('express');
+const path=require('path');
 const app = express();
+app.use(express.static('./navbar-app'))
+//app.get('/',(req,res)=>{
+    
+//res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+//})
 
-app.get('/',(req,res)=>{
-res.send('hello World')
-})
-app.get('/about',(req,res)=>{
-    res.send('Home Page')
-})
 app.all('*',(req,res)=>{
     res.status(404).send("<h1>Resource not found</h1>")
 })
