8f608ebd0183e93de5ebacb7a379f3d8e0a76fcf
sai2963 <venkatsai0629@gmail.com>
2024-01-13 00:35:42 +0530

    parmams added

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express=require('express');
-const path=require('path');
 const app = express();
-
+const{products}=require('./data')
+app.get('/',(req,res)=>{
+    res.send('<h1>Home</h1><a href="api/products">products</a>')
+})
+app.get('/api/products',(req,res)=>{
+    const newproducts=products.map((product)=>{
+        const {id,name,image}=product;
+        return{id,name,image}
+    })
+    res.json(newproducts);
+})
 
 app.listen(5000,()=>{
     console.log('server is runnimg at port 5000')
