8768863eaea537f25b5b60feb6f1a15042f6169d
sai2963 <venkatsai0629@gmail.com>
2024-01-13 01:02:49 +0530

    route parmams added

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const express=require('express');
+const express = require("express");
 const app = express();
-const{products}=require('./data')
-app.get('/',(req,res)=>{
-    res.send('<h1>Home</h1><a href="api/products">products</a>')
-})
-app.get('/api/products/:productID',(req,res)=>{
-    console.log(req);
-    console.log(req.params);
-    const singleproduct =products.find((product)=> product.id===1)
-    res.json(singleproduct);
-})
+const { products } = require("./data");
+app.get("/", (req, res) => {
+  res.send('<h1>Home</h1><a href="api/products">products</a>');
+});
+app.get("/api/products/:productID", (req, res) => {
+  //console.log(req);
+  //console.log(req.params);
+  const { productID } = req.params;
+  const singleproduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  res.json(singleproduct);
+});
 
-app.listen(5000,()=>{
-    console.log('server is runnimg at port 5000')
-}) 
+app.listen(5000, () => {
+  console.log("server is runnimg at port 5000");
+});
