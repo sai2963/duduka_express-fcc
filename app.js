9043d6069568bad2830d04d3a830efdb6c527c0c
sai2963 <venkatsai0629@gmail.com>
2024-01-14 02:05:06 +0530

    methods get used  added

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const logger=require('./logger.js')
-const authorize=require('./authorize.js')
-//app.use(authorize,logger)
-app.use(morgan('tiny'));
-app.get('/',(req,res)=>{
-  res.send('Home')
+const {people}=require('./data.js');
+
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({ extended: false }))
+app.get('/api/people',(req,res)=>{
+  res.status(404).json({success:true,data:people})
 })
-app.get('/about',(req,res)=>{
-  res.send('about')
-})
-app.get('/api/products',(req,res)=>{
-  res.send("api")
-})
-app.get('/api/products/cpu',(req,res)=>{
-  res.send("computer")
+
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+
+  res.status(401).send('Please Provide Credentials')
 })
 app.listen(5000, () => {
   console.log("server is runnimg at port 5000");
